c7c8b86bc3bd859e699e9b63a6dc710348ac7e81
Nischal Shrestha <nischalstha14@gmail.com>
2023-10-13 19:14:09 +1100

    update

---
+++ client/src/Login/Login.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import login from './Login.module.scss'
+import logo from '../files/logo/fedlogo.png';
+import {useNavigate} from 'react-router-dom'
+export default function Login() {
+
+  const navigate = useNavigate();
+  const mentor =()=>{
+    navigate('/mentor_login')
+  }
+  
+  return (
+    <div className={login.main}>
+        <div className={login.left}>
+          <div className={login.logo}>
+            <img src={logo} alt='logo'/>
+          </div>
+        </div>
+        <div className={login.right}>
+          <div className={login.title}><h1>Mentrix</h1></div>
+          <div className={login.selection}>
+            <div className={login.text}><p>Are you a</p></div>
+            <div className={login.button}><button>Student</button></div>
+            <div className={login.text}><p>or</p></div>
+            <div className={login.button}><button onClick={mentor}>Mentor</button></div>
+          </div>
+        </div>
+    </div>
+  )
+}
