89d999b05273c29913b50696a902e26363d99e70
Nischal Shrestha <nischalstha14@gmail.com>
2023-10-15 08:49:14 +1100

    update

--- client/src/Components/Main/Main.jsx
+++ client/src/Components/Main/Main.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import main from './Main.module.scss'
 import s from '../../files/Images/1.jpg'
+import aa from '../../files/Images/aa.png'
+import aaa from '../../files/Images/aaa.png'
+import demo from '../../files/demo.json'
+
+
 function Main() {
+
+    
+
   return (
     <div className={main.main}>
         <div className={main.write}>
@@ -16,15 +24,19 @@ function Main() {
             <div className={main.userinfo}>
                 <div className={main.userimgbox}><img src={s} alt='userimg'/></div>
                 <div className={main.username}>
-                    <span className={main.name}>Inajic Baldwin</span>
+                    <span className={main.name}>{demo.name}</span>
                     <span className={main.time}>Posted 5 min ago</span>
                 </div>
             </div>
-            <div className={main.description}>Can someone help me to solve this shit</div>
+            <div className={main.description}>Can somasdasdasadadseone help me tadadasdasdadasdasdadadsadadadasdasdo solve this shit</div>
             <div className={main.image}>
-                <img src='' alt='userpost'/>
+                <img src={aa} alt='userpost'/>
             </div>
-            <div className={main.btns}></div>
+            <div className={main.btns}>
+                <div><button className={main.btn}>Help</button></div>
+                <div><button className={main.btn}> Share</button></div>
+
+            </div>
 
         </section>
     </div>
